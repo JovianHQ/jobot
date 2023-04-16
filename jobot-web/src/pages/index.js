c3d1dd1625193f3b13257614473c1dc1eef6fdd4
Aakash N S <aakashns@jovian.ai>
2023-04-16 16:06:47 +0000

    add more functionality

--- jobot-web/src/pages/index.js
+++ jobot-web/src/pages/index.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import Head from "next/head";
+import { createParser } from "eventsource-parser";
+import TextareaAutosize from "react-textarea-autosize";
 
 const SYSTEM_MESSAGE =
   "You are Jobot, a helpful and verstaile AI created by Jovian using state-of the art ML models and APIs.";
@@ -16,34 +18,103 @@ export default function Home() {
 
   const API_URL = "https://api.openai.com/v1/chat/completions";
 
-  async function sendRequest() {
-    // update the message history
-    const newMessage = { role: "user", content: userMessage };
-    const newMessages = [...messages, newMessage];
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      sendRequest();
+    }
+  };
 
-    setMessages(newMessages);
+  const sendRequest = async () => {
+    if (!userMessage) {
+      alert("Please enter a message before you hit send");
+    }
+
+    if (!apiKey) {
+      alert(
+        "Please provide your OpenAI API key in the navbar. Get it from https://platform.openai.com . NOTE: Your API key is never sent to our server."
+      );
+      return;
+    }
+
+    const oldUserMessage = userMessage;
+    const oldMessages = messages;
+
+    const updatedMessages = [
+      ...messages,
+      {
+        role: "user",
+        content: userMessage,
+      },
+    ];
+
+    setMessages(updatedMessages);
     setUserMessage("");
 
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + apiKey,
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: newMessages,
-      }),
-    });
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: updatedMessages,
+          stream: true,
+        }),
+      });
 
-    const responseJson = await response.json();
+      if (response.status !== 200) {
+        throw new Error(
+          `OpenAI API returned an error. Please ensure you've provided the right API key. Check the "Console" or "Network" of your browser's developer tools for details.`
+        );
+      }
 
-    const newBotMessage = responseJson.choices[0].message;
+      const reader = response.body.getReader();
 
-    const newMessages2 = [...newMessages, newBotMessage];
+      let newMessage = "";
+      const parser = createParser((event) => {
+        if (event.type === "event") {
+          const data = event.data;
+          if (data === "[DONE]") {
+            return;
+          }
+          const json = JSON.parse(event.data);
+          const content = json.choices[0].delta.content;
 
-    setMessages(newMessages2);
-  }
+          if (!content) {
+            return;
+          }
+
+          newMessage += content;
+
+          const updatedMessages2 = [
+            ...updatedMessages,
+            { role: "assistant", content: newMessage },
+          ];
+
+          setMessages(updatedMessages2);
+        } else {
+          return "";
+        }
+      });
+
+      // eslint-disable-next-line
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        const text = new TextDecoder().decode(value);
+        parser.feed(text);
+      }
+    } catch (error) {
+      console.error("error");
+
+      setUserMessage(oldUserMessage);
+      setMessages(oldMessages);
+      window.alert("Error:" + error.message);
+    }
+  };
 
   return (
     <>
@@ -85,16 +156,20 @@ export default function Home() {
 
         {/* Message Input Box */}
         <div>
-          <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4">
-            <textarea
+          <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4 items-start">
+            <TextareaAutosize
               value={userMessage}
+              autoFocus
+              maxRows={10}
+              onKeyDown={handleKeyDown}
+              placeholder="Ask me anything.."
               onChange={(e) => setUserMessage(e.target.value)}
-              className="border text-lg rounded-md p-1 flex-1"
+              className="border text-lg rounded-md p-2 flex-1 resize-none"
               rows={1}
             />
             <button
               onClick={sendRequest}
-              className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-1 ml-2"
+              className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-2 ml-2"
             >
               Send
             </button>
