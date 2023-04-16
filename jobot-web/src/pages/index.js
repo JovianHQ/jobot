715063f6d52944a0d72fa9bf8cb3fbc5e23d419a
Aakash N S <aakashns@jovian.ai>
2023-04-16 15:03:20 +0000

    add head

--- jobot-web/src/pages/index.js
+++ jobot-web/src/pages/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
+import Head from "next/head";
 
 const SYSTEM_MESSAGE =
   "You are Jobot, a helpful and verstaile AI created by Jovian using state-of the art ML models and APIs.";
@@ -45,56 +46,61 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col h-screen">
-      {/* Navigation Bar */}
-      <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
-        <div className="text-xl font-bold">Jobot</div>
+    <>
+      <Head>
+        <title>Jobot - Your friendly neighborhood AI</title>
+      </Head>
+      <div className="flex flex-col h-screen">
+        {/* Navigation Bar */}
+        <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
+          <div className="text-xl font-bold">Jobot</div>
+          <div>
+            <input
+              type="password"
+              className="border p-1 rounded"
+              onChange={(e) => setApiKey(e.target.value)}
+              value={apiKey}
+              placeholder="Paste API Key here"
+            />
+          </div>
+        </nav>
+
+        {/* Message History */}
+        <div className="flex-1 overflow-y-scroll ">
+          <div className="w-full max-w-screen-md mx-auto px-4">
+            {messages
+              .filter((message) => message.role !== "system")
+              .map((message, idx) => (
+                <div key={idx} className="my-3">
+                  <div className="font-bold">
+                    {message.role === "user" ? "You" : "Jobot"}
+                  </div>
+                  <div className="text-lg prose">
+                    <ReactMarkdown>{message.content}</ReactMarkdown>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+
+        {/* Message Input Box */}
         <div>
-          <input
-            type="password"
-            className="border p-1 rounded"
-            onChange={(e) => setApiKey(e.target.value)}
-            value={apiKey}
-            placeholder="Paste API Key here"
-          />
-        </div>
-      </nav>
-
-      {/* Message History */}
-      <div className="flex-1 overflow-y-scroll ">
-        <div className="w-full max-w-screen-md mx-auto px-4">
-          {messages
-            .filter((message) => message.role !== "system")
-            .map((message, idx) => (
-              <div key={idx} className="my-3">
-                <div className="font-bold">
-                  {message.role === "user" ? "You" : "Jobot"}
-                </div>
-                <div className="text-lg prose">
-                  <ReactMarkdown>{message.content}</ReactMarkdown>
-                </div>
-              </div>
-            ))}
+          <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4">
+            <textarea
+              value={userMessage}
+              onChange={(e) => setUserMessage(e.target.value)}
+              className="border text-lg rounded-md p-1 flex-1"
+              rows={1}
+            />
+            <button
+              onClick={sendRequest}
+              className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-1 ml-2"
+            >
+              Send
+            </button>
+          </div>
         </div>
       </div>
-
-      {/* Message Input Box */}
-      <div>
-        <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4">
-          <textarea
-            value={userMessage}
-            onChange={(e) => setUserMessage(e.target.value)}
-            className="border text-lg rounded-md p-1 flex-1"
-            rows={1}
-          />
-          <button
-            onClick={sendRequest}
-            className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-1 ml-2"
-          >
-            Send
-          </button>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
