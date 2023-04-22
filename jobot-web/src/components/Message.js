e5da700e1c165a3e4980a252fef6e800151cf1a7
Aakash N S <aakashns@jovian.ai>
2023-04-22 08:53:01 +0000

    add system avatar

--- jobot-web/src/components/Message.js
+++ jobot-web/src/components/Message.js
@@ -5,7 +5,13 @@ const Message = ({ role, content }) => {
   return (
     <div className="my-4 mx-auto flex w-full max-w-4xl ">
       <Image
-        src={role === "user" ? "/profile_placeholder.png" : "/jobot.jpg"}
+        src={
+          role === "user"
+            ? "/profile_placeholder.png"
+            : role === "assistant"
+            ? "/jobot.jpg"
+            : "/jovian_avatar.jpeg"
+        }
         height={40}
         width={40}
         alt="Avatar"
@@ -15,7 +21,7 @@ const Message = ({ role, content }) => {
       <div className="flex-1 overflow-x-hidden pl-2">
         <div>
           <span className="text-base font-medium">
-            {role === "user" ? "You " : "Jobot "}
+            {role === "user" ? "You " : role === "system" ? "System" : "Jobot "}
           </span>
         </div>
 
