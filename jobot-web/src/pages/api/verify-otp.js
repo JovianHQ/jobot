f2ac5f6a67bc1b5376f84d621e57c466768f380b
Aakash N S <aakashns@jovian.ai>
2023-04-27 13:22:36 +0000

    just show user

--- jobot-web/src/pages/api/verify-otp.js
+++ jobot-web/src/pages/api/verify-otp.js
@@ -52,47 +52,49 @@ export default async function handler(req, res) {
     user = data1?.user;
   }
 
-  if (!user) {
-    console.error("unable to retrieve user");
-    return new Response("Unable to retrieve user", {
-      status: 400,
-      headers,
-    });
-  }
+  return new Response(JSON.stringify({ user }), { headers, status: 200 });
 
-  const keyName = "API Key - " + new Date().toString();
+  // if (!user) {
+  //   console.error("unable to retrieve user");
+  //   return new Response("Unable to retrieve user", {
+  //     status: 400,
+  //     headers,
+  //   });
+  // }
 
-  const keyData = {
-    user_id: user.id,
-    name: keyName,
-  };
+  // const keyName = "API Key - " + new Date().toString();
 
-  const { error: error3 } = await supabase.from("apikeys").insert(keyData);
+  // const keyData = {
+  //   user_id: user.id,
+  //   name: keyName,
+  // };
 
-  if (error3) {
-    return new Response("Failed to create API key. " + error3.message, {
-      status: 500,
-      headers,
-    });
-  }
+  // const { error: error3 } = await supabase.from("apikeys").insert(keyData);
 
-  const { data, error: error4 } = await supabase
-    .from("apikeys")
-    .select("*")
-    .eq("name", keyName)
-    .eq("user_id", user.id)
-    .single();
+  // if (error3) {
+  //   return new Response("Failed to create API key. " + error3.message, {
+  //     status: 500,
+  //     headers,
+  //   });
+  // }
 
-  if (error4) {
-    return new Response("Failed to retrieve API key. " + error3.message, {
-      status: 500,
-      headers,
-    });
-  }
+  // const { data, error: error4 } = await supabase
+  //   .from("apikeys")
+  //   .select("*")
+  //   .eq("name", keyName)
+  //   .eq("user_id", user.id)
+  //   .single();
 
-  headers["Content-Type"] = "application/json";
-  return new Response(JSON.stringify({ apiKey: data }), {
-    status: 200,
-    headers,
-  });
+  // if (error4) {
+  //   return new Response("Failed to retrieve API key. " + error3.message, {
+  //     status: 500,
+  //     headers,
+  //   });
+  // }
+
+  // headers["Content-Type"] = "application/json";
+  // return new Response(JSON.stringify({ apiKey: data }), {
+  //   status: 200,
+  //   headers,
+  // });
 }
